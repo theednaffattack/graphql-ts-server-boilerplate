bab024d7231820d221c185126edf639b7fb2ecc1
eddie naff <eddienaff@eddienaff.com>
2019-03-27 08:42:16 -0700

    Clean logs out of  tests

--- src/modules/me/me.test.ts
+++ src/modules/me/me.test.ts
@@ -2,21 +2,15 @@ import axios from "axios";
 import { createTypeOrmConn } from "../../utils/createTypeormConnection";
 import { User } from "../../entity/User";
 
-// const redis = new Redis();
 import { Connection } from "typeorm";
-
-// let userId: any;
 
 let connection: Connection;
 const email = "esad@mac.com";
 const password = "kasdjfksafdj";
 
-// const goodEmail = "eddienaff@gmail.com";
-// const goodPassword = "booyakasha";
-
 export const loginAndQueryMeTest = async () => {
   test("get currrent user", async done => {
-    const loginThing = await axios.post(
+    await axios.post(
       process.env.TEST_HOST as string,
       {
         query: loginMutation(email, password)
@@ -26,46 +20,30 @@ export const loginAndQueryMeTest = async () => {
       }
     );
 
-    const response = await axios.post(
+    await axios.post(
       process.env.TEST_HOST as string,
       { query: meQuery },
       { withCredentials: true }
     );
 
-    console.log("From test: response.data");
-    console.log("loginThing.headers");
-    console.log(loginThing.headers);
-    console.log("response.headers");
-    console.log(response.headers);
-    console.log(response.data.data);
-
-    console.log(Object.keys(response.headers));
-    console.log(Object.keys(loginThing.headers));
     done();
   });
 };
 
 beforeAll(async () => {
   if (connection) {
-    console.log("CONNECTED ALREADY!");
-    const user = await User.create({
+    await User.create({
       email,
       password,
       confirmed: true
     }).save();
-
-    console.log("VIEW user");
-    console.log(user);
   } else {
     connection = await createTypeOrmConn();
-    const user = await User.create({
+    await User.create({
       email,
       password,
       confirmed: true
     }).save();
-
-    console.log("VIEW user");
-    console.log(user);
   }
 });
 
@@ -94,12 +72,8 @@ mutation {
 `;
 
 describe("me", () => {
-  //   test("can't get user if not logged in", async () => {
-  //     // later
-  //   });
-
   test("get currrent user", async done => {
-    const loginThing2 = await axios.post(
+    await axios.post(
       process.env.TEST_HOST as string,
       {
         query: loginMutation(email, password)
@@ -109,23 +83,13 @@ describe("me", () => {
       }
     );
 
-    const response2 = await axios.post(
+    await axios.post(
       process.env.TEST_HOST as string,
       { query: meQuery },
       { withCredentials: true }
     );
 
-    console.log("From test: response.data");
-    console.log("loginThing.headers");
-    console.log(loginThing2.headers);
-    console.log("response.headers");
-    console.log(response2.headers);
-    console.log(response2.data.data);
-
-    console.log(Object.keys(response2.headers));
-    console.log(Object.keys(loginThing2.headers));
-    // done();
-    const loginThing = await axios.post(
+    await axios.post(
       process.env.TEST_HOST as string,
       {
         query: loginMutation(email, password)
@@ -135,21 +99,12 @@ describe("me", () => {
       }
     );
 
-    const response = await axios.post(
+    await axios.post(
       process.env.TEST_HOST as string,
       { query: meQuery },
       { withCredentials: true }
     );
 
-    console.log("From test: response.data");
-    console.log("loginThing.headers");
-    console.log(loginThing.headers);
-    console.log("response.headers");
-    console.log(response.headers);
-    console.log(response.data.data);
-
-    console.log(Object.keys(response.headers));
-    console.log(Object.keys(loginThing.headers));
     done();
   });
 });
